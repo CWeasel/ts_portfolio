5233d1f87955bb57869ff84f44d03903a6c7b4df
CWeasel <crystal.weasel@gmail.com>
2026-04-06 22:40:21 +0100

    Prepping folder for minimal go live

--- frontend/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ["localhost", "127.0.0.1"],
-  devIndicators: false,
-};
-
-export default nextConfig;
